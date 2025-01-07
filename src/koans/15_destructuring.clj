fc12b714dc3ad5657c8bb75e2045ac76330501dd
Aaron Brown <aaron.brown@droneup.com>
2025-01-07 14:02:00 -0700

    destructuring

--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -6,19 +6,18 @@
    :city "Testerville"
    :state "TX"})
 
-
-
 (meditations
   "Destructuring is an arbiter: it breaks up arguments"
-  (= ":bar:foo" ((fn [[a b]]
-          (str b a))
+  (= ":bar:foo" ((fn [[a b]] (str b a))
          [:foo :bar]))
 
   "Whether in function definitions"
   (= (str "An Oxford comma list of apples, "
           "oranges, "
           "and pears.")
-     ((fn [[a b c]] (str "An Oxford comma list of " a ", " b ", and " c "."))
+     ((fn [[a b c]] (str "An Oxford comma list of " a ", "
+                         b ", "
+                         "and " c "."))
       ["apples" "oranges" "pears"]))
 
   "Or in let expressions"
@@ -30,18 +29,20 @@
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
      (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
-       {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}))
+       {:original-parts [first-name last-name] :named-parts {:first first-name :last last-name}}))
 
   "Break up maps by keys"
   (= "123 Test Lane, Testerville, TX"
      (let [{street-address :street-address, city :city, state :state} test-address]
-       "123 Test Lane, Testerville, TX"))
+       (str street-address ", " city ", " state)))
 
   "Or more succinctly"
   (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address & remainder]} test-address]
-       "123 Test Lane, Testerville, TX"))
+     (let [{:keys [street-address city state]} test-address]
+       (str street-address ", " city ", " state)))
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (let ["Test" "Testerson"] test-address)))
+      ((fn [[first last] {:keys [street-address city state]}]
+                (str first " " last ", " street-address ", " city ", " state))
+                ["Test" "Testerson"] test-address) ))
